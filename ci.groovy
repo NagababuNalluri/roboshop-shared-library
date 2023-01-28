c8c92cea5d253b73381a5a9fcd405487dc13db17
nagababu <nag4c1@gmail.com>
2023-01-28 07:22:28 +0530

    updating ./roboshop-shared-library

--- ci.groovy
+++ ci.groovy
@@ -1,40 +1,42 @@
- pipeline  {
+ def call() {
+     pipeline  {
 
-    agent {
-        label 'workstation'
-    }
-    stages  {
-        stage('Compile/Build') {
-            steps {
-                echo 'hello from hello compile stage'
-            }
-        }
-
-
-
-        stage('Unit tests') {
-            steps{
-                echo 'hello from hello unit stage'
-            }
-        }
+         agent {
+             label 'workstation'
+         }
+         stages  {
+             stage('Compile/Build') {
+                 steps {
+                     echo 'hello from hello compile stage'
+                 }
+             }
 
 
-        stage('Quality control') {
-            steps{
-                echo 'hello from hello quality stage'
-            }
-        }
+
+             stage('Unit tests') {
+                 steps{
+                     echo 'hello from hello unit stage'
+                 }
+             }
 
 
-        stage('Storing in centralized palce') {
-            steps{
-                echo 'hello from hello centralised stage'
-            }
-        }
-    }
-    post{
-        always {
-            echo 'send an email'
-        }
-    }
+             stage('Quality control') {
+                 steps{
+                     echo 'hello from hello quality stage'
+                 }
+             }
+
+
+             stage('Storing in centralized palce') {
+                 steps{
+                     echo 'hello from hello centralised stage'
+                 }
+             }
+         }
+         post{
+             always {
+                 echo 'send an email'
+             }
+         }
+     }
  }
