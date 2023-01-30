bdc4573b34ba592c6afbe3a0cc1ea7fa50f3e8f8
nagababu <nag4c1@gmail.com>
2023-01-30 12:47:34 +0530

    updating ./roboshop-shared-library

--- vars/test.groovy
+++ vars/test.groovy
@@ -17,4 +17,11 @@ def list =["father","mother","wife","daughter"]
     for(i in list) {
         println(i)
     }
+    def a=5
+    def b=10
+    if(a = b){
+        print "a and b are same values"
+    }else{
+        print "a and b are not having same value"
+    }
 }
