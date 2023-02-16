f89974c76b0a1cae6a3594b9665dd08e2e3ee50f
nagababu <nag4c1@gmail.com>
2023-02-16 11:28:41 +0530

    updating ./roboshop-shared-library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -6,12 +6,12 @@ def call() {
             }
         }
         parameters {
-            string(name: 'INFRA-ENV', defaultValue: '', description: 'Input as dev or prod')
+            string(name: 'INFRA_ENV', defaultValue: '', description: 'Input as dev or prod')
         }
         stages {
-            stage('terraform init') {
+            stage ('terraform Init') {
                 steps {
-                    sh "terraform init -backend-config=env-${'INFRA-ENV'}/state.tfvars"
+                    sh "terraform init -backend-config=env-${INFRA_ENV}/state.tfvars"
                 }
 
 
