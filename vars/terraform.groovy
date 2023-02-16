3f6d2de2c1feffd24e326536cb47166ed7d2dbf9
nagababu <nag4c1@gmail.com>
2023-02-16 19:45:01 +0530

    configured frontend microservice

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -20,7 +20,7 @@ def call() {
             stage ('terraform Apply') {
                 steps {
 
-                     sh "terraform apply -auto-approve -var-file=env-${INFRA_ENV}/main.tfvars"
+                     sh "terraform plan -auto-approve  -var-file=env-${INFRA_ENV}/main.tfvars"
                 }
             }
 
